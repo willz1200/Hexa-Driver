460b161f1622481262796c97bdb92aee5bb95504
willz1200 <wrbednall@yahoo.co.uk>
2019-10-24 21:54:56 +0100

    Even more lines

--- Firmware/Hexa/Hexa.ino
+++ Firmware/Hexa/Hexa.ino
@@ -6,7 +6,7 @@
  * @Date		12/04/2019 (Last Updated)
  * @Author(s)	William Bednall
  ******************************************************************************/
-//some line, nice
+//some line, nice things
  
 #include "LinearActuator.h"
 
