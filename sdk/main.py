63b0f619b248bca5ae22e26600dc3eb960512d09
Russell Grim <russellgrim@gmail.com>
2019-11-20 21:20:56 +0000

    add serial read

--- sdk/main.py
+++ sdk/main.py
@@ -1,1 +1,13 @@
-test
+import serial 
+
+
+ser = serial.Serial('COM4')
+ser.baudrate = 115200
+
+# ser.port = 'COM4'
+while True:
+    if ( ser.inWaiting() ):
+        print(ser.readline())   # read a '\n' terminated line)         # check which port was really used
+# ser.write(b'hello')     # write a string
+# ser.close()             # close port
+
