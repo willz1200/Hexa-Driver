73961b7847cac74767ba38ffedf2be1a17590f3a
willz1200 <wrbednall@yahoo.co.uk>
2019-11-24 11:56:03 +0000

    Created first version of the SDK GUI

--- sdk/main.py
+++ sdk/main.py
@@ -1,74 +1,169 @@
 # *******************************************************************************
 # * @File       main.py
 # * @Brief      SDK for controlling the Hexa Driver and graphing data.
-# * @Date       20/11/2019 (Last Updated)
-# * @Author(s)  Russell Grim
+# * @Date       24/11/2019 (Last Updated)
+# * @Author(s)  William Bednall, Russell Grim
 # *******************************************************************************
 
 import serial
 import time
-import matplotlib.pyplot as plt
+import sys
+import numpy as np
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtCore, QtGui, uic
+from PyQt4.QtGui import QFileDialog
+# import matplotlib.pyplot as plt
 
-ser = serial.Serial('COM5')
-#ser = serial.Serial('COM11')
-ser.baudrate = 115200
-old_input = None
-ser.write(b'z 1\r') #Sets to sdk mode. So it dosn't echo all commands.
-# ser.port = 'COM4'while True:
+class HexaSDK(QtGui.QMainWindow):
 
-time = []
-position = []
-velocity = []
+    def __init__(self):
+        self.ser = serial.Serial('COM11')
+        self.ser.baudrate = 115200
+        self.checkWait = False
 
-while True:
-    # time.sleep(1)
-    
-    with open('input.txt', 'r') as file:  # Use file to refer to the file object
-        new_input = float(file.readline())
-    if (new_input != old_input):
-        old_input = new_input
-        if (old_input == 0):
-            ser.write(b'led 0\r') 
-            print("turn led off")
+        self.ser.write(b'z 1\r') # Sets to sdk mode. So it dosn't echo all commands.
+        self.ser.write(b'v 1\r') # Enable position and velocity streaming
 
-        elif (old_input == 1):
-            ser.write(b'led 255\r')     # write a string
-            print ("turn led on")
-        elif (old_input == 2):
-            ser.write(b'ls\r') 
-            print ("request sensor")
-        elif (old_input == 3):
-            ser.write(b'v 0\r') 
-            print ("request sensor")
-            break 
-        elif (old_input == 4):
-            ser.write(b'v 1\r') 
-            print ("request sensor") 
-    if ( ser.inWaiting() ):
-        line = ser.readline()   # read a '\n' terminated line)
-        line = line.decode('utf-8')
-        line = line.replace("\r\n","")
-        line = line.split(',')
-        if (line[0] == 's'):
-            time.append(float(line[1]))
-            position.append(float(line[2]))
-            velocity.append(float(line[3]))
-        print(line)
-    
-    
-        # print (type(float(line[0])))
-        # print (line.decode('utf-8').split(','))
-        # breakpoint()
-# breakpoint()
+        super(HexaSDK, self).__init__() # The super() builtin returns a proxy object that allows you to refer parent class by 'super'.
+        uic.loadUi("gui.ui", self)
+        self.velPosGraphInit(self.widget)
+        # self.velPosGraphInit(self.myWidget)
 
-# ser.close()             # close port
-if __name__ == "__main__":
-    fig, ax = plt.subplots()
-    ax.plot(time, position)
-    ax.plot(time, velocity)
-    ax.set_title('Title')
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.legend(('position', 'velocity'))
-    plt.show()
-    breakpoint()
+        self.inoLaunchPathDialog.clicked.connect(self.selectFile)
+        self.sendCommand.clicked.connect(self.sendCommandB)
+
+        self.togPosVelStreamData.stateChanged.connect(self.togglePosVelStreamData)
+        self.togPosVelStreamData.toggle() # Start as ticked
+
+        self.togSDKmode.stateChanged.connect(self.toggleSDKmode)
+        self.togSDKmode.toggle() # Start as ticked
+
+        entries = ['Off', 'PI', 'Timed - Sweep', 'Timed - Single']
+        self.listView_ControllerMode.addItems(entries)
+        self.listView_ControllerMode.selectionModel().selectionChanged.connect(self.controllerMode)
+
+        self.btnTimeBasedDemo.clicked.connect(self.timeBasedDemo)
+        self.btnTimeBasedOpen.clicked.connect(self.timeBasedOpen)
+        self.btnTimeBasedClosed.clicked.connect(self.timeBasedClosed)
+
+    def selectFile(self):
+        self.inoFilePath.setText(QFileDialog.getOpenFileName())
+
+    def sendCommandB(self):
+        self.ser.write(str(self.enterCommand.text()) + "\r")
+        self.enterCommand.setText("")
+
+    def togglePosVelStreamData(self):
+        if self.togPosVelStreamData.isChecked():
+            self.ser.write(b'v 1\r') # Enable position and velocity streaming
+        else:
+            self.ser.write(b'v 0\r') # Disable position and velocity streaming
+
+    def toggleSDKmode(self):
+        if self.togSDKmode.isChecked():
+            self.ser.write(b'z 1\r') # Sets to sdk mode. So it dosn't echo all commands.
+        else:
+            self.ser.write(b'z 0\r')
+
+    def timeBasedDemo(self):
+        self.ser.write(b'rt 2\r')
+        self.ser.write(b'rt s 500 1 75\r')
+        time.sleep(0.500)
+        self.ser.write(b'rt s 1000 2 75\r')
+        time.sleep(1)
+        self.ser.write(b'rt s 750 1 75\r')
+        time.sleep(0.75)
+        self.ser.write(b'rt 0\r')
+
+    def timeBasedOpen(self):
+        self.ser.write(b'rt 2\r')
+        self.ser.write(b'rt s 500 1 75\r')
+        time.sleep(0.500)
+        self.ser.write(b'rt 0\r')
+
+    def timeBasedClosed(self):
+        self.ser.write(b'rt 2\r')
+        self.ser.write(b'rt s 500 2 75\r')
+        time.sleep(0.500)
+        self.ser.write(b'rt 0\r')
+
+    def controllerMode(self):
+        controllerRow = self.listView_ControllerMode.currentRow()
+        if controllerRow == 0:
+            self.ser.write(b'r 0\r')
+        elif controllerRow == 1:
+            self.ser.write(b'r 1\r')
+        elif controllerRow == 2:
+            self.ser.write(b'rt 1\r')
+        elif controllerRow == 3:
+            self.ser.write(b'rt 2\r')
+            self.ser.write(b'rt s 500 1 75\r')
+
+    def taskTimer(self):
+        if (self.checkWait):
+            print("Test")
+
+    def keyPressEvent(self, event):
+        #super(myGUI, self).keyPressEvent(event)
+        if (event.key() == QtCore.Qt.Key_Enter) or (event.key() == QtCore.Qt.Key_Return):
+            self.sendCommandB()
+        event.accept()
+
+    def velPosGraphInit(self, graph):
+        # self.myWidget = pg.PlotWidget()
+        graph.setLabel('left', 'Encoder Counts', units='')
+        graph.setLabel('bottom', 'Time', units='s')
+        graph.addLegend()
+        # Use automatic downsampling and clipping to reduce the drawing load
+        graph.setDownsampling(mode='peak')
+        graph.setClipToView(True)
+        graph.setRange(xRange=[-500, 0])
+        graph.setLimits(xMax=0)
+
+        self.curve = graph.plot(pen='g', name='Position')
+        self.curveB = graph.plot(pen='r', name='Velocity')
+        self.data = np.empty(500)
+        self.dataB = np.empty(500)
+        self.ptr = 0
+
+        timer = pg.QtCore.QTimer(self)
+        timer.timeout.connect(self.velPosGraphUpdate)
+        timer.start(5)
+
+        timer = pg.QtCore.QTimer(self)
+        timer.timeout.connect(self.taskTimer)
+        timer.start(1)
+
+    def velPosGraphUpdate(self):
+        if (self.ser.inWaiting()):
+            line = self.ser.readline()   # read a '\n' terminated line)
+            line = line.decode('utf-8')
+            line = line.replace("\r\n","")
+            self.historyCommand.append(line) # add text to command box
+            line = line.split(',')
+            if (line[0] == 's'):
+                self.data[self.ptr] = float(line[2])#np.random.normal()
+                self.dataB[self.ptr] = float(line[3])#np.random.normal()
+                self.ptr += 1
+                if self.ptr >= self.data.shape[0]:
+                    tmp = self.data
+                    tmpB = self.dataB
+                    self.data = np.empty(self.data.shape[0] * 2)
+                    self.dataB = np.empty(self.dataB.shape[0] * 2)
+                    self.data[:tmp.shape[0]] = tmp
+                    self.dataB[:tmpB.shape[0]] = tmpB
+                self.curve.setData(self.data[:self.ptr])
+                self.curve.setPos(-self.ptr, 0)
+                self.curveB.setData(self.dataB[:self.ptr])
+                self.curveB.setPos(-self.ptr, 0)
+
+if __name__ == '__main__':
+    if QtGui.QApplication.instance() is None:
+        app = QtGui.QApplication(sys.argv)
+
+    ObjHexaSDK = HexaSDK()
+    ObjHexaSDK.show()
+    # ObjHexaSDK.start()
+    self = ObjHexaSDK
+    exitCode = app.exec_() # Will block until application is closed
+    sys.exit(exitCode)
