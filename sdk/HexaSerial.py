37ede84da389644bd98b3ea993b391f92e8cd41a
willz1200 <wrbednall@yahoo.co.uk>
2019-12-23 17:33:40 +0000

    Added data structure to link identifiers to queues

--- sdk/HexaSerial.py
+++ sdk/HexaSerial.py
@@ -13,67 +13,87 @@ import serial
 ser = serial.Serial("COM11")
 ser.baudrate = 115200
 
+# Multiple queue to sort incoming data
 qGraphA = Queue()
 qGraphB = Queue()
 qMisc = Queue()
-qOutGoing = Queue()
+
+# Single queue to hold outgoing data
+qOutgoing = Queue()
+
+# Create an array of dictionaries for directing incoming data into the correct queue
+dataIdentifiers = [
+    # Link line identifiers to correct queues below
+    { 'id':'s', 'queue':qGraphA },
+    { 'id':'p', 'queue':qGraphB },
+
+    # Make sure DEFAULT_QUEUE is always the last item in this list
+    { 'id':'DEFAULT_QUEUE', 'queue':qMisc }
+]
 
 def enqueue_incomingData():
-	global ser
+    global ser
 
-	while(1):
-		if (ser.inWaiting()):
-			rawLine = ser.readline()   # read a '\n' terminated line)
-			rawLine = rawLine.decode('utf-8')
-			rawLine = rawLine.replace("\r\n","")
-			splitLine = rawLine.split(',')
-			if (splitLine[0] == 's'):
-				qGraphA.put(rawLine)
-			elif (splitLine[0] == 'p'):
-				qGraphB.put(rawLine)
-			else:
-				qMisc.put(rawLine)
+    while(1):
+        if (ser.inWaiting()):
+            rawLine = ser.readline()   # read a '\n' terminated line)
+            rawLine = rawLine.decode('utf-8')
+            rawLine = rawLine.replace("\r\n","")
+            splitLine = rawLine.split(',')
 
-def unqueue_outGoingData():
-	global ser
+            queueFound = False
 
-	while(1):
-		try:
-			lineOut = qOutGoing.get_nowait() # Remove and return an item from the queue
-		except Empty:
-			pass # The queue is empty, do nothing
-		else:
-			lineOut = lineOut.decode('ascii')
-			print(lineOut)
-			#ser.write(b'v 1\r')
-			ser.write( ("{}\r").format(lineOut).encode() )
+            for i in dataIdentifiers: 
+                if (i['id'] == splitLine[0]):
+                    queueFound = True
+                    i['queue'].put(rawLine)
+                    break
+                elif (i['id'] == "DEFAULT_QUEUE" and queueFound == False):
+                    i['queue'].put(rawLine)
+
+def unqueue_outgoingData():
+    global ser
+
+    while(1):
+        try:
+            lineOut = qOutgoing.get_nowait() # Remove and return an item from the queue
+        except Empty:
+            pass # The queue is empty, do nothing
+        else:
+            lineOut = lineOut.decode('ascii')
+            ser.write( ("{}\r").format(lineOut).encode() )
 
 def sendData(data):
-	qOutGoing.put(data)
+    qOutgoing.put(data)
 
 def getData(queue):
-	try:
-		lineTest = queue.get_nowait()
-	except Empty:
-		pass # The queue is empty, do nothing
-	else:
-		lineTest = lineTest.decode('ascii')
-		return lineTest
+    try:
+        lineTest = queue.get_nowait()
+    except Empty:
+        pass # The queue is empty, do nothing
+    else:
+        lineTest = lineTest.decode('ascii')
+        return lineTest
 
 
 tIncoming = Thread(target=enqueue_incomingData)
 tIncoming.daemon = True
 tIncoming.start()
 
-tOutgoing = Thread(target=unqueue_outGoingData)
+tOutgoing = Thread(target=unqueue_outgoingData)
 tOutgoing.daemon = True
 tOutgoing.start()
 
+sendData("z 0") # SDK mode off
+sendData("v 1") # Stream data on
+
 while(1):
-	time.sleep(2)
-	print ( "{}, {}, {}".format( qGraphA.qsize(), qGraphB.qsize(), qMisc.qsize() ) )
-	sendData("led 150")
-	
-	myData = getData(qMisc)
-	if (myData != None):
-		print(myData)
+    time.sleep(2)
+    print ( "{}, {}, {}".format( qGraphA.qsize(), qGraphB.qsize(), qMisc.qsize() ) )
+    
+    sendData("led 150")
+
+    myData = getData(qMisc)
+    if (myData != None):
+        print(myData)
+  
