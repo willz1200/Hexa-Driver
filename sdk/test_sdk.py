6cf2adf544b67dc5df99dcd912c13a01e56274f4
Russell Grim <russellgrim@gmail.com>
2020-03-30 22:14:23 +0100

    fix unit testing

--- sdk/test_sdk.py
+++ sdk/test_sdk.py
@@ -5,3 +5,11 @@ from sdk.HexaSDK import *
 
 def test_led():
     assert True
+
+def test_freqency_responce():
+    HEXA_SDK = HexaSDK()
+    HEXA_SDK.isEchoCommandsOn = True
+    HEXA_SDK.setLinearActuatorWorkspace(0)
+    HEXA_SDK.setLinearActuator(0, True)
+    HEXA_SDK.setControllerMode(HEXA_SDK.mode.off)
+    HEXA_SDK.frequencyResponce(1)
